34b3531867113f1305be6e1e4e496d3954a075f7
woojoo123 <woozoo0926@naver.com>
2025-12-28 00:20:47 +0900

    post클래스 추가

---
+++ src/main/java/com/example/domain/Post.java
@@ -0,0 +1,24 @@
+
+public class Post {
+    private final long id;
+    private final String title;
+    private final String content;
+
+    public Post(long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
