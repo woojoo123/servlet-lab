daa03de9b4cf513d6c2ff991b2ce8fbeeb422098
woojoo123 <woozoo0926@naver.com>
2025-12-28 00:33:38 +0900

    PostStore를 스레드 안전버전으로 교체

--- src/main/java/com/example/repo/PostStore.java
+++ src/main/java/com/example/repo/PostStore.java
@@ -2,17 +2,33 @@ package com.example.repo;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+import com.example.domain.Post;
 public class PostStore {
     
     // 단순 실습용 (서버 메모리에 저장)
-    private static final List<String> posts = new ArrayList<>();
+    // private static final List<String> posts = new ArrayList<>();
     
-    public static void add(String title, String content) {
-        posts.add("title=" + title + ", content=" + content);
+    // public static void add(String title, String content) {
+    //     posts.add("title=" + title + ", content=" + content);
+    // }
+
+    // public static List<String> findAll() {
+    //     return new ArrayList<>(posts);
+    // }
+
+    private static final AtomicLong seq = new AtomicLong(0);
+    private static final CopyOnWriteArrayList<Post> posts = new CopyOnWriteArrayList<>();
+
+    public static Post add(String title, String content) {
+        long id = seq.incrementAndGet();
+        Post p = new Post(id, title, content);
+        posts.add(p);
+        return p;
     }
 
-    public static List<String> findAll() {
+    public static List<Post> findAll() {
         return new ArrayList<>(posts);
     }
 }
